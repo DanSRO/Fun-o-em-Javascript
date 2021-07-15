34d9b466b609da1579ab7df535761ca091a2b975
daniel dantas <dan_dantas@hotmail.com>
2021-07-14 22:56:44 -0300

    commit da funcao JS

---
+++ funcao.js
@@ -0,0 +1,12 @@
+// 3 formas de declarar o mesmo código de uma mesma função;
+function somar(a,b){
+    return a+b;
+}
+let resultado=somar(10,15);
+console.log(resultado);
+//---------------
+// const somar=function(a,b){return a+b;}
+// console.log(somar(10,15));
+//----------------
+//const somar=(a,b)=> a+b;
+//console.log(somar(10,15));
